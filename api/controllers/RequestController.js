f75119276676f2834ff1cbeae974d547847eb29c
Gonçalo Margalho <gsky89@gmail.com>
2014-10-01 21:12:28 +0200

    modified request controller

--- api/controllers/RequestController.js
+++ api/controllers/RequestController.js
@@ -6,11 +6,10 @@
  */
 
 module.exports = {
-    invite: function(req, res, nect) {
+    invite: function(req, res, next) {
         var params = req.params.all();
         RequestManager.invite(params.email, req.session.user, function (err, data) {
             res.send({type: data.type, text: data.text, name: data.name, reference: data.reference});
         });
     }
 };
-
